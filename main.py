ebcd6b3c6e91b9ea54eba79db0fd02f1ead7ef9c
BMarx981 <b.marx981@gmail.com>
2019-11-13 12:38:15 -0500

    "textfield data put in place. Blank space gives /"

--- main.py
+++ main.py
@@ -11,22 +11,27 @@ from RISParser import RISParser
 
 app = Flask(__name__)
 rp = RISParser()
+ta = ''.format('UTF-8')
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', ta=ta)
 
 @app.route("/success", methods=['GET', 'POST'])
 def success():
+    global ta
     if request.method == 'POST':
         tags = str(request.form['textData']).split(' ')
         tags = [x.replace('\\n', '').replace(',', '') for x in tags]
         tags = list(filter(None, tags))
+        ta = ''
+        for s in tags:
+            ta += s + ' '
         f = request.files['uploadFile']
         content = str(f.read().decode("utf-8-sig").encode("utf-8")).split('\\n')
         content = list(filter(None, content))
         rp.processFile(content, tags, f.filename)
-        return render_template('processing.html')
+        return render_template('processing.html', f=f.filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
